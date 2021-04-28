4a41eb74514151bbf0b066c3a8d8b5930ec9f2db
Γεώργιος Αλεξάκης <58263228+Looper2074@users.noreply.github.com>
2021-04-28 23:59:13 +0300

    Update main.js
    Added the ability to parse arguments. I splice the array at index 2 to only keep the integers from the parsed arguments. Kept only the best solution, cleaned the code, used better names for variables.

--- main.js
+++ main.js
@@ -1,37 +1,10 @@
-var test = [1278234,39426311,434341,23,423,423];
+var array = process.argv.slice(2, process.argv.length);
 var phonetics = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-console.time('benchmark1')
-var result = "";
-for (var i=0;i<test.length;i++) {
-    var number = test[i].toString();
-    for (var j = 0; j < number.length; j++) result+=phonetics[number[j]];
-    if(i<test.length-1) result = result + ",";
-}
-
-console.timeEnd('benchmark1');
-console.log(result);
-
-console.time('benchmark2')
-
-var result1 = test.reduce(myFunc,[]);
-function myFunc(total,number,index)
-{
-    var sep="";
-    if(index < test.length-1) sep=",";
-    return total+(""+number).split("").reduce((acc, cur,index) => {
-        return acc+phonetics[cur]},"")+sep;
-}
-
-console.timeEnd('benchmark2');
-
-console.log(result1);
-console.time('benchmark3');
-
-var result2 = test.reduce(myFunc2,[]);
-function myFunc2(total,number,index)
+var result = array.reduce(FindSolution,[]);
+function FindSolution(total,number,index)
 {
     var comma='';
-    if(index<test.length-1)comma=",";
+    if(index<array.length-1)comma=",";
     return total+getPhonetics(number,[])+comma;
 }
 function getPhonetics(number,acc){
@@ -41,6 +14,4 @@ function getPhonetics(number,acc){
     }
     return acc;
 }
-
-console.timeEnd('benchmark3');
-console.log(result2);
+process.stdout.write(result+'\n');
